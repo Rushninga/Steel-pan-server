6cc80e8984eeac202af4788f3676d6bac7181101
Rushes-Art <shjankie@salcc.edu.lc>
2024-11-02 16:34:54 -0400

    Login functionality
    adds login functionality

--- networking.gd
+++ networking.gd
@@ -1,51 +1,46 @@
 extends Node
-var id
-var username = "shernan"
-var password = "password"
-var connected_ids = []
-var list_connected_user_info = {
-	"username" : [],
-	"id" : []
-}
-var list_user_info = {
-	"username" : ["shernan"],
-	"password" : ["password"],
-}
-
+@onready var connected_ids = $Node2D.connected_ids
+@onready var list_connected_user_info = $Node2D.list_connected_user_info
+@onready var db = $Node2D.db
+var query:String
+var bindings:Array 
 
 
 
 func _ready():
-	$Node2D.send_id.connect(add_id)
+	$Node2D.connect("login_confirm", _login_confirm)
 
 
-func add_id(id):
-	connected_ids.append(id)
-
+func _login_confirm():
+	
+	pass
 
 
 @rpc("any_peer", "reliable", "call_local")
-func send_user_info(username_recieved, password_received, mode_received):
+func send_user_info(username_recieved, password_recieved, mode_recieved):
 	if multiplayer.is_server():
 		var id = multiplayer.get_remote_sender_id()
-		if mode_received == "sign in":
+		if mode_recieved == "sign in":
 			
 			pass
 		else:
-			var list_count = 0
-			for i in list_user_info["username"]:
-				if username_recieved == i and password_received == list_user_info["password"][list_count]:
-					rpc_id(id, "user_login_confirm")
-				list_count += 1
+			bindings = [username_recieved, password_recieved]
+			query = "SELECT username,password FROM user_info WHERE username = ? AND password = ?;"
+			db.query_with_bindings(query, bindings)
+			var results = db.query_result
+			var message
+			if results.is_empty(): #sends a login confirmation to client, 1 == username or password is incorrect, 1 is login was sucessful and 3 is unknown error 
+				message = 1
+			elif results.size() == 1:
+				message = 2
+				list_connected_user_info["username"] = username_recieved
+				list_connected_user_info["id"] = multiplayer.get_remote_sender_id()
+			else:
+				message = 3
+			user_login_confirm.rpc_id(id,message)
 			
-			
-			if username_recieved == username and password_received == password:
-				
-				pass
 		
-		list_connected_user_info["username"] = username_recieved
-		list_connected_user_info["id"] = multiplayer.get_remote_sender_id()
 		
 @rpc("authority", "unreliable_ordered", "call_remote")
-func user_login_confirm():
+func user_login_confirm(message):
 	pass
--- node_2d.gd
+++ node_2d.gd
@@ -3,26 +3,71 @@ var server = ENetMultiplayerPeer.new()
 var host_address 
 var default_port = 8000
 var port
-signal send_id
-
-# Called when the node enters the scene tree for the first time.
+var db = SQLite.new()
+var db_is_open = false
+@onready var db_query = $HBoxContainer/VBoxContainer2/SQL_Query
+@onready var print_result = $HBoxContainer/VBoxContainer2/Label
+var query
+var list_connected_user_info = {
+	"username" : [],
+	"id" : []
+}
+var connected_ids = []
+signal login_confirm
 func _ready():
 	multiplayer.peer_connected.connect(_on_player_connected)
-	
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	db.path = "res://Main.db"
 	pass # Replace with function body.
 
 
-func _on_button_pressed():
+func _on_button_pressed(): #starts server
 	server.create_server(default_port)
 	multiplayer.multiplayer_peer = server
 	host_address = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")),1)
-	$VBoxContainer/Label.text = host_address
-	
+	$HBoxContainer/VBoxContainer/Label.text += host_address + "\n"
 
 
 func _on_player_connected(id):
-	$VBoxContainer/Label.text += "\nPlayer " 
-	$VBoxContainer/Label.text += str(id)
-	$VBoxContainer/Label.text += " has connected"
-	emit_signal("send_id", id)
-	pass
+	$HBoxContainer/VBoxContainer/Label.text += "\nPlayer " 
+	$HBoxContainer/VBoxContainer/Label.text += str(id)
+	$HBoxContainer/VBoxContainer/Label.text += " has connected"
+	connected_ids.append(id)
+
+	
+
+func _on_player_disconnected(id):
+	$HBoxContainer/VBoxContainer/Label.text += "\nPlayer " 
+	$HBoxContainer/VBoxContainer/Label.text += str(id)
+	$HBoxContainer/VBoxContainer/Label.text += " has disconnected"
+	for i in connected_ids:
+		if i == id:
+			connected_ids.erase(i)
+	
+
+func _on_button_2_pressed(): #opens database
+	db.open_db()
+	db.foreign_keys = true
+	db.query("SELECT 1")
+	#Checks if database open successfully with sql query
+	if db.error_message == "not an error":
+		$HBoxContainer/VBoxContainer/Label.text += "Database opened successfully \n"
+	else:
+		$HBoxContainer/VBoxContainer/Label.text += "Database has failed to open \n"
+	
+	
+
+
+func _on_print_pressed(): #process an sql query and prints the result
+	query = $HBoxContainer/VBoxContainer2/SQL_Query.text
+	db.query(query)
+	$HBoxContainer/VBoxContainer2/Label.text = ""
+	for i in db.query_result:
+		$HBoxContainer/VBoxContainer2/Label.text += str(i)
+		$HBoxContainer/VBoxContainer2/Label.text += "\n"
+	pass # Replace with function body.
+
+
+func _on_clear_pressed():
+	$HBoxContainer/VBoxContainer/Label.text = ""
+	pass # Replace with function body.
